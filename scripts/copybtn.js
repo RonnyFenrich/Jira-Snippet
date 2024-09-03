efaeadeab60a353d66ed307bf6ec0609be60bd59
Ronny Fenrich <ronny@distillmobile.com>
2024-09-03 10:39:25 -0600

    Add new hardcoded button to copy branch name, fix getIssueId() when using search

--- scripts/copybtn.js
+++ scripts/copybtn.js
@@ -5,8 +5,13 @@ function getIssueId() {
   let issueKey;
 
   if (document.URL.includes(searchParam)) {
-    let match = document.URL.match(/\&selectedIssue=(.*?)(?=&|$)/)[1];
-    issueKey = match;
+    if (document.URL.match(/\?selectedIssue=(.*?)(?=&|$)/)) {
+      let match = document.URL.match(/\?selectedIssue=(.*?)(?=&|$)/)[1]
+      issueKey = match;
+    } else if (document.URL.match(/\&selectedIssue=(.*?)(?=&|$)/)) {
+      let match = document.URL.match(/\&selectedIssue=(.*?)(?=&|$)/)[1];
+      issueKey = match;
+    }
   } else {
     let match = document.title.match(/\[(.*?)\]/)[1];
     issueKey = match;
@@ -44,19 +49,83 @@ function getIssueDataAndWriteToClipboard(issueId)
         .replaceAll('{reporter}', issueReporter)
         .replaceAll('{assignee}', issueAssignee)
         .replaceAll('{url}', issueUrl)
-      
+
         navigator.clipboard.writeText(outputText);
     });
   });
 }
+
+// Variation of getIssueDataAndWriteToClipboard with hardcoded template and additional logic
+function createButtonCopyBranchNameGetIssueDataAndWriteToClipboard(issueId) {
+  const restCallForIssue = `${window.location.origin}/rest/api/3/issue/`;
+
+  fetch(`${restCallForIssue}${issueId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const issueKey = data['key'];
+      const issueTitle = data['fields']['summary'];
+      const issueDescription = data['fields']['description'];
+      const issueType = data['fields']['issuetype'].name;
+      const issuePriority = data['fields']['priority']?.name;
+      const issueStatus = data['fields']['status'].name;
+      const issueReporter = data['fields']['reporter'].displayName;
+      const issueAssignee = data['fields']['assignee'] ? data['fields']['assignee'].displayName : 'Unassigned';
+      const issueUrl = `${window.location.origin}/browse/${issueId}`;
+      const specialBranchName = issueTitle
+        .replace(/\([\s\S]*?\)/g, '')
+        .replace(/[^a-zA-Z ]/g, '')
+        .toLowerCase()
+        .replace(/[-_\s.]+(.)?/g, (_, c) => c ? c.toUpperCase() : '');
+
+      const format = '{key}-{specialBranchName}';
+      const outputText = format
+        .replaceAll('{key}', issueKey)
+        .replaceAll('{title}', issueTitle)
+        .replaceAll('{description}', issueDescription)
+        .replaceAll('{type}', issueType)
+        .replaceAll('{priority}', issuePriority)
+        .replaceAll('{status}', issueStatus)
+        .replaceAll('{reporter}', issueReporter)
+        .replaceAll('{assignee}', issueAssignee)
+        .replaceAll('{url}', issueUrl)
+        .replaceAll('{specialBranchName}', specialBranchName)
+
+        navigator.clipboard.writeText(outputText);
+    });
+}
+
 
 function notify(message)
 {
   getIssueDataAndWriteToClipboard(message.issueId);
 }
 
+// Button to create useful branch name
+// Template is hardcoded: {key}-{CamelCaseJiraTitleWithoutPrefixTagsInBraces}
+// Example: AB-12344-ImprovedTitleOfJiraTicket
+function createButtonCopyBranchName(parent) {
+  const buttonText = '📋 Branch';
+  const button = document.createElement("button");
+  button.textContent = buttonText;
+  button.id = "createButtonCopyBranchName";
+  button.className = "CopyBtnForJira";
+  parent.appendChild(button);
+
+  button.onclick = function() {
+    const issueId = getIssueId();
+    if (issueId == null) {
+      button.textContent = 'Error: No Issue id found!';
+    }
+    createButtonCopyBranchNameGetIssueDataAndWriteToClipboard(issueId);
+    button.textContent = '✅';
+    setTimeout(function() {
+      button.textContent = buttonText;
+    }, 2000);
+  };
+}
+
 function createButton(parent) {
-  const buttonText = 'Jira Snippet';
+  const buttonText = '📋 Jira';
   const button = document.createElement("button");
   button.textContent = buttonText;
   button.id = "CopyBtnJiraId";
@@ -69,7 +138,7 @@ function createButton(parent) {
           button.textContent = 'Error: No Issue id found!';
         }
         getIssueDataAndWriteToClipboard(issueId);
-        button.textContent = 'Text has been copied!';
+        button.textContent = '✅';
         setTimeout(function () {
           button.textContent = buttonText;
         }, 2000);
@@ -81,6 +150,10 @@ var observer = new MutationObserver(function (mutations, me) {
                document.getElementsByClassName('_otyr1y44 _ca0q1y44 _u5f3idpf _n3td1y44 _19bvidpf _1e0c116y')[0] ??
                document.getElementsByClassName('_otyr1b66 _1yt4swc3 _1e0c116y')[0];
   if (parent) {
+    if (!document.getElementById('createButtonCopyBranchName')) {
+      createButtonCopyBranchName(parent);
+    }
+
     if (!document.getElementById('CopyBtnJiraId')) {
       createButton(parent);
     }
